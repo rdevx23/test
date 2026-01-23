50a10df751eb02d20ce3baf9c630d5a33baee3d7
rdevx23 <clasherkayt@gmail.com>
2026-01-23 03:31:40 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/0ED7y3nV/raw"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/gui2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/git.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/git2.lua"))()
