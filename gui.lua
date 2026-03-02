0252a73604f70aaf1615e27a3344ba9555773c88
rdevx23 <clasherkayt@gmail.com>
2026-03-02 22:49:48 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,3 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/git.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/git2.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rdevx23/test/refs/heads/main/git3.lua"))()
